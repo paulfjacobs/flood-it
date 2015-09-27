d4273ed99a056d2a10aa746c3dc2989e5ce85dcb
Paul Jacobs <paul.f.jacobs@gmail.com>
2015-09-26 21:40:26 -0400

    The initial click/color counter display and basic updating logic. Also commented out some excessive logging.

--- js/Game.js
+++ js/Game.js
@@ -8,6 +8,8 @@ var FI = FI || {};
 // a little weird having this up here but we need to be able to inform the grid about the color selection
 // TODO: Probably refactor; maybe have Game be a proper class
 var grid;
+var counter;
+var total_clicks_allowed;
 
 function startUp() {
     console.log("Start Up!");
@@ -43,9 +45,29 @@ function startUp() {
         // then append it to the select element
         select.appendChild(opt);
     }
+
+    // click counter
+    counter = 0;
+    var click_counter = document.getElementById("clickCounter");
+    click_counter.innerHTML = counter;
+
+    // TODO: calculate real value
+    total_clicks_allowed = 22;
+    var total_clicks_allowed_counter = document.getElementById("clickTotalAllowed");
+    total_clicks_allowed_counter.innerHTML = total_clicks_allowed;
 }
 
 function colorSelected() {
+    // update counter logic; determine if the game is done
+    var click_counter = document.getElementById("clickCounter");
+    click_counter.innerHTML = counter+1;
+    counter++;
+
+    if(counter > total_clicks_allowed) {
+        console.log("Lost");
+        // TODO: popup
+    }
+
     var select = document.getElementById("colorSelect");
     var color = select.options[select.selectedIndex].text;
     console.log("Color selected function called with this color:"+color);
@@ -53,4 +75,6 @@ function colorSelected() {
     // propagate change on grid
     grid.colorSelected(color, 0, 0);
     grid.draw();
+
+    // TODO: determine if the user won
 }
--- js/Grid.js
+++ js/Grid.js
@@ -34,23 +34,23 @@ FI.Grid.prototype.draw = function() {
 // this is the recursive call; the idea is to do a BFS search; search down and right; if that color is equal to your
 // current color OR the color that you are changing to then also color the function with that Box's row and col
 FI.Grid.prototype.colorSelected = function(color, row, col) {
-    console.log("colorSelect() with color:"+color+" row:"+row+" col:"+col);
+    //console.log("colorSelect() with color:"+color+" row:"+row+" col:"+col);
     var currentColor = this.data[row][col].getColor();
-    console.log("Current Color:"+currentColor+" numRowsAndCols:"+this.numRowsAndCols);
+    //console.log("Current Color:"+currentColor+" numRowsAndCols:"+this.numRowsAndCols);
 
     // check children; down and right
     if(row+1 < this.numRowsAndCols &&
         (this.data[row+1][col].getColor() === currentColor || this.data[row+1][col].getColor() == color)) {
-        console.log("Checking down...");
+        //console.log("Checking down...");
         this.colorSelected(color, row+1, col);
     }
     if(col+1 < this.numRowsAndCols &&
         (this.data[row][col+1].getColor() === currentColor || this.data[row][col+1].getColor() == color)) {
-        console.log("Checking to the right...");
+        //console.log("Checking to the right...");
         this.colorSelected(color, row, col+1);
     }
 
     // now change my color
-    console.log("Change the color of the Box at row:"+row+" col:"+col+" to color:"+color);
+    //console.log("Change the color of the Box at row:"+row+" col:"+col+" to color:"+color);
     this.data[row][col].setColor(color);
 }
